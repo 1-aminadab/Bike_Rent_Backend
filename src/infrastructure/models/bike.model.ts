5cd016bc4f597b6ac055b87a31ec0f8cbee1ad33
1-aminadab <developer.amanuel@gmail.com>
2024-08-17 19:46:37 +0300

    add necessary folders for the ride

---
+++ src/infrastructure/models/bike.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface IBike extends Document {
+  bikeId: string;
+  isAvailable: boolean;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+  inUse: boolean;
+}
+
+const BikeSchema: Schema = new Schema({
+  bikeId: { type: String, required: true, unique: true },
+  isAvailable: { type: Boolean, required: true, default: true },
+  location: {
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true }
+  },
+  inUse: { type: Boolean, required: true, default: false }
+});
+
+export const BikeModel = mongoose.model<IBike>('Bike', BikeSchema);
